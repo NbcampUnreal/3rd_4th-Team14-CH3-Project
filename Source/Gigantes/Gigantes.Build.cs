faf9efd054e82cf2a1ba02e37bfd2b1120c39d54
kmy9317 <kimmin9632@gmail.com>
2025-07-28 21:40:20 +0900

    Initial Setup
    Init Project

---
+++ Source/Gigantes/Gigantes.Build.cs
@@ -0,0 +1,37 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Gigantes : ModuleRules
+{
+	public Gigantes(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "NavigationSystem",
+			"GameplayTasks", "GameplayTags", "UMG" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PublicIncludePaths.AddRange(new string[]
+		{
+			"Source/Gigantes/Character",
+			"Source/Gigantes/GameModes",
+			"Source/Gigantes/Player",
+			"Source/Gigantes/Items",
+			"Source/Gigantes/UI",
+			"Source/Gigantes/AI",
+			"Source/Gigantes/Weapons",
+			"Source/Gigantes/Input"
+		});
+		
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+	
+}
---
+++ Source/Gigantes/Gigantes.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Gigantes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Gigantes, "Gigantes" );
