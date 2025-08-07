aabc732a9f096c859f66aae82b3f551b34e3eee7
suaio239 <72058199+suaio239@users.noreply.github.com>
2025-08-08 02:59:55 +0900

    [chore](Item): DTO 구조체 cpp 삭제
    구조체의 cpp 파일 미사용 및 PCH 자동 생성 빌드 에러 해소

--- Source/Gigantes/Items/Structs/FGtItemData.cpp
+++
@@ -1,2 +0,0 @@
-#include "C:\Users\suaio\gitHub\3rd_4th-Team14-CH3-Project\Intermediate\Build\Win64\x64\GigantesEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
-#include "FGtItemData.h"
