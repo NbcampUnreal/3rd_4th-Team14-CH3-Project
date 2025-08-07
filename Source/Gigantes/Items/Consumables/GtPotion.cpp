b2d98b2aaa188455de848fccf0e552f876f38901
suaio239 <72058199+suaio239@users.noreply.github.com>
2025-08-08 03:09:19 +0900

    [chore](Item): 파생 클래스 초기화
    아직 개발하지 않은 파생 클래스 더미빌드 에러 해소차 생성자 제외 모든 필드 및 함수 초기화

--- Source/Gigantes/Items/Consumables/GtPotion.cpp
+++ Source/Gigantes/Items/Consumables/GtPotion.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GtPotion.h"
 
 
@@ -8,19 +5,5 @@
 AGtPotion::AGtPotion()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 }
-
-// Called when the game starts or when spawned
-void AGtPotion::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AGtPotion::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
-
--- Source/Gigantes/Items/Weapons/GtSniper.cpp
+++ Source/Gigantes/Items/Weapons/GtSniper.cpp
@@ -5,19 +5,7 @@
 AGtSniper::AGtSniper()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 }
 
-// Called when the game starts or when spawned
-void AGtSniper::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AGtSniper::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
 
