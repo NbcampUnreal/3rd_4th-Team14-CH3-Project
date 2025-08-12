b80adcb55dc3ebc01bf70972ca0f059a53191147
Tae-Hoon Kim <ajtmxm1114@naver.com>
2025-08-12 21:28:16 +0900

    [feat](module_ai_char_base_item)메인메뉴 수정
    메인메뉴의 start버튼을 눌러도 플레이어 컨트롤이 안돼는 문제 수정
    
    헤드샷 득점체크 추가

--- Source/Gigantes/GameModes/GtGameModeBase.h
+++ Source/Gigantes/GameModes/GtGameModeBase.h
@@ -16,21 +16,42 @@ public:
 	virtual void Tick(float DeltaTime) override;  //틱
 
 	UFUNCTION(BlueprintCallable, Category = "Game")
-	void AddScore(int32 Points);  //점수추가
+	void AddScore(int32 Points, bool bHeadshot = false);  //점수추가(헤드샷 파라미터 추가)
 	
 	UFUNCTION(BlueprintCallable, Category = "Game")
-	void EnemyKilled();  //적처치시
+	void EnemyKilled(bool bHeadshot = false);  //적처치시(헤드샷 파라미터 추가)
 
 	UFUNCTION(BlueprintCallable, Category = "Game")
 	void PlayerDied();  //플레이어 사망시
 
 protected:
+	
+	// Delegate 선언 (헤더에 추가)
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnScoreUpdated, int32, NewScore);
+
+	// 추가: 체력 변화 델리게이트 (UI 바인딩용)
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnPlayerHealthChanged, float, OldHealth, float, NewHealth);
+	UPROPERTY(BlueprintAssignable, Category = "Game Events")
+	FOnPlayerHealthChanged OnPlayerHealthChanged;
+
+	// 추가: 총알 변화 델리게이트 (UI 바인딩용)
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerAmmoChanged, int32, NewAmmo);
+	UPROPERTY(BlueprintAssignable, Category = "Game Events")
+	FOnPlayerAmmoChanged OnPlayerAmmoChanged;
+	
+	// 이벤트 디스패처 추가 (UI 업데이트용, 블루프린트에서 바인딩 가능)
+	UPROPERTY(BlueprintAssignable, Category = "Game Events")
+	FOnScoreUpdated OnScoreUpdated;  // 점수 업데이트 이벤트 (Delegate 추가 예시)
+	
 	UPROPERTY(EditDefaultsOnly, Category = "Game")
 	int32 MaxEnemies = 10;  //클리어에 필요한 적의 갯수
 
 	UPROPERTY(EditDefaultsOnly, Category = "Game")
 	float GameTimeLimit = 300.0f;  //시간초과로 게임오버되는 제한시간
 
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<class UUserWidget> HUDWidgetClass;
+	
 	int32 CurrentEnemiesKilled = 0; //현재처치한적갯수
 	float ElapsedTime = 0.0f; //경과시간
 
@@ -38,4 +59,7 @@ protected:
 	bool bGameCleared = false;
 
 	void EndGame(bool bWon);  //게임종료
+
+	
+
 };
--- Source/Gigantes/GameModes/GtGameStateBase.h
+++ Source/Gigantes/GameModes/GtGameStateBase.h
@@ -16,6 +16,9 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "Game")
 	int32 RemainingEnemies = 10;  //남은적 갯수
 
+	UPROPERTY(BlueprintReadOnly, Category = "Game")
+	float ElapsedTime = 0.0f;  // 경과 시간 (UI 노출용 추가)
+	
 	UFUNCTION(BlueprintCallable, Category = "Game")
 	int32 GetScore() const { return CurrentScore; }
 };
